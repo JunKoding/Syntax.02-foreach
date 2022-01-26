ea48f7034a1e0d9020cde5cf9ce53fa7ebb45f75
JunKoding <wnsrb0407@daum.net>
2022-01-26 14:05:57 +0900

    스위프트 기초 문법- 02. foreach 반복문

--- 02.playground/Contents.swift
+++ 02.playground/Contents.swift
@@ -14,7 +14,7 @@ for item in myArray1 {
 }
 // --> myArray가 가지고 있는 11개의 요소를 item이라는 변수에 담아서 처리하겠다는 뜻.
 // 특정한 조건을 담아서 처리할 수도 있다. 특정한 조건을 담으려면 where를 쓴다.
-
+// item:0 , ..... item: 10
 
 
 //ex1)
@@ -23,21 +23,22 @@ var myArray2: [Int] = [0,1,2,3,4,5,6,7,8,9,10]
 for item in myArray2 where item > 5  {
     print("5보다 큰 수: \(item)")
 }
-
+// 5보다 큰 수: 6 , ..... item: 10
 
 
 //ex2)
-var myArray3: [Int] = [0,1,2,3,4,5,6,7,8,9,10]
+var myArray3: [Int] = [1,2,3,4,5,6,7,8,9,10]
 
 for item in myArray3 where item % 2 == 0 {
     print("짝수: \(item)")
 }
-
+// 짝수:2 , ..... 짝수: 10
 
 
 //ex3)
-var myArray4: [Int] = [0,1,2,3,4,5,6,7,8,9,10]
+var myArray4: [Int] = [1,2,3,4,5,6,7,8,9,10]
 
 for item in myArray4 where item % 2 != 0 {
     print("홀수: \(item)")
 }
+// 홀수: 1, ..... 홀수: 9
